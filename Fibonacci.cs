1421cef5b1fb4c552aea0e661dd00dcdc6de0fa5
Bogdan <wasd.solana@gmail.com>
2025-05-30 13:18:48 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = 10; 
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write($"{a} ");
+
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
